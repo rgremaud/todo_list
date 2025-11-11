d77089a3915f2f438f11b2182915ac8dffb3da7f
rgremaud <rgremau@gmail.com>
2025-11-10 22:04:49 -0500

    Add header

---
+++ src/header.js
@@ -0,0 +1,6 @@
+export { createHeader }
+
+function createHeader() { 
+    const header = document.getElementById("header");
+    header.textContent = "Todoodoo"
+}
--- src/index.js
+++ src/index.js
@@ -5,18 +5,19 @@ import "./styles.css";
 import { createProjectDialog } from "./dialog.js";
 import { Database } from "./database.js";
 import { addProjectClickEvent, createProjectButton } from "./button.js"
+import { createHeader } from "./header.js";
 import { Project } from "./projects.js"
 import { printScreen } from "./display.js";
 
 /*
 Todo list:
-- Add a header section w/page title
-- Add a sidebar that lists projects
-- Create center console area for tracking todos
+- Add a circle box at the start of each todo which is red if completed, green if not completed
 */
 
 // initialize the site
 function initialize() {
+    createHeader();
+
     const projectDatabase = new Database();
 
     const newDialog = createProjectDialog("addProject");
