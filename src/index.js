5118df975c3803579d2eb841b6ae31e1e796be10
Romain Gremaud <rgremau@gmail.com>
2025-11-20 19:26:04 -0500

    Update index

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import { createHeader } from "./header.js";
 import { createSidebar } from "./sidebar.js";
 import { loadDatabase } from "./storage.js";
 /*
-
 Add option to adjust priority on projects
 Add option to assign to-do date on projects
 Add option to adjust to-do date
