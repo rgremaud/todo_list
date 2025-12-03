fef3972988f23f785fac966df9e79e9f28e617ef
rgremaud <rgremau@gmail.com>
2025-12-02 20:14:39 -0500

    Refactor the project dialog function

--- src/dialog.js
+++ src/dialog.js
@@ -6,99 +6,16 @@ import { submitTodoClickEvent } from "./button.js";
 import cancelSvg from "./assets/cancel.svg";
 
 function createProjectDialog(dialogId) {
-  const projectDialog = document.createElement("dialog");
-  projectDialog.id = dialogId;
+  const projectDialog = createDialog(dialogId);
 
-  const projectForm = document.createElement("form");
+  const projectForm = createForm();
 
-  const lineOne = document.createElement("div");
-  lineOne.className = "formLine";
-  const projectTitle = document.createElement("label");
-  projectTitle.textContent = "Project Title: ";
+  const formLines = createProjectFormLines(projectDialog);
 
-  const titleInput = document.createElement("input");
-  titleInput.id = "projectTitle";
-  projectTitle.htmlFor = "projectTitle";
-
-  lineOne.appendChild(projectTitle);
-  lineOne.appendChild(titleInput);
-
-  const lineTwo = document.createElement("div");
-  lineTwo.className = "formLine";
-  const projectDescription = document.createElement("label");
-  projectDescription.textContent = "Description: ";
-
-  const descriptionInput = document.createElement("input");
-  descriptionInput.id = "projectDescription";
-
-  projectDescription.htmlFor = "projectDescription";
-
-  lineTwo.appendChild(projectDescription);
-  lineTwo.appendChild(descriptionInput);
-
-  const lineThree = document.createElement("div");
-  lineThree.className = "formLine";
-  const projectPriority = document.createElement("label");
-  projectPriority.textContent = "Priority: ";
-
-  const priorityInput = document.createElement("select");
-  priorityInput.id = "projectPriority";
-
-  const priorityOptions = [
-    { value: "high", text: "High" },
-    { value: "medium", text: "Medium" },
-    { value: "low", text: "Low" },
-  ];
-
-  priorityOptions.forEach((option) => {
-    const priorityOption = document.createElement("option");
-    priorityOption.value = option.value;
-    priorityOption.textContent = option.text;
-    priorityInput.appendChild(priorityOption);
-  });
-
-  projectPriority.htmlFor = "projectPriority";
-
-  lineThree.appendChild(projectPriority);
-  lineThree.appendChild(priorityInput);
-
-  const lineFour = document.createElement("div");
-  lineFour.className = "formLine";
-  const projectDueDate = document.createElement("label");
-  projectDueDate.textContent = "Due date: ";
-
-  const dueDateInput = document.createElement("input");
-  dueDateInput.id = "projectDueDate";
-  dueDateInput.type = "date";
-
-  lineFour.appendChild(projectDueDate);
-  lineFour.appendChild(dueDateInput);
-
-  const lineFive = document.createElement("div");
-  lineFive.classList = ("formLine", "buttonLine");
-  const addProjectButton = document.createElement("button");
-  addProjectButton.id = "addProjectButton";
-  addProjectButton.textContent = "Add Project";
-
-  lineFive.appendChild(addProjectButton);
-
-  const cancelButton = document.createElement("button");
-
-  const cancelSvgButton = document.createElement("img");
-  cancelSvgButton.src = cancelSvg;
-  cancelSvgButton.alt = "Cancel";
-
-  cancelButton.appendChild(cancelSvgButton);
-
-  closeDialog(cancelButton, projectDialog);
-
-  lineOne.appendChild(cancelButton);
-
-  projectForm.appendChild(lineOne);
-  projectForm.appendChild(lineTwo);
-  projectForm.appendChild(lineThree);
-  projectForm.appendChild(lineFour);
-  projectForm.appendChild(lineFive);
+  formLines.forEach((line) => {
+    projectForm.appendChild(line);
+    line.className = "formLine";
+  })
 
   projectDialog.appendChild(projectForm);
 
@@ -107,6 +24,48 @@ function createProjectDialog(dialogId) {
   content.appendChild(projectDialog);
 
   return projectDialog;
+}
+
+function createProjectFormLines(dialog) { 
+  const formLines = []
+
+  const priorityOptions = [
+    { value: "high", text: "High" },
+    { value: "medium", text: "Medium" },
+    { value: "low", text: "Low" },
+  ];
+
+  const lineOne = createTextBoxInput("Project Title: ", "projectTitle");
+  formLines.push(lineOne);
+  const lineTwo = createTextBoxInput("Description: ", "projectDescription");
+  formLines.push(lineTwo);
+  const lineThree = createSelectInput("Priority: ", "projectPriority", priorityOptions)
+  formLines.push(lineThree)
+  const lineFour = createDateInput("Due date: ", "projectDueDate");
+  formLines.push(lineFour)
+  const lineFive = document.createElement("div");
+  formLines.push(lineFive)
+  lineFive.classList = ("formLine", "buttonLine");
+  const addProjectButton = document.createElement("button");
+  addProjectButton.id = "addProjectButton";
+  addProjectButton.textContent = "Add Project";
+
+  lineFive.appendChild(addProjectButton);
+
+  // refactor this outside
+  const cancelButton = document.createElement("button");
+
+  const cancelSvgButton = document.createElement("img");
+  cancelSvgButton.src = cancelSvg;
+  cancelSvgButton.alt = "Cancel";
+
+  cancelButton.appendChild(cancelSvgButton);
+
+  closeDialog(cancelButton, dialog);
+
+  lineOne.appendChild(cancelButton);
+
+  return formLines;
 }
 
 function createTodoDialog(project, database) {
@@ -272,4 +231,76 @@ function editProjectDialog(headerDiv, dialogId, project, database) {
   headerDiv.appendChild(editProjectDialog);
 
   return editProjectDialog;
+}
+
+function createDialog(id="") { 
+  const dialog = document.createElement("dialog");
+  dialog.id = id;
+
+  return dialog;
+}
+
+function createForm(id="") {
+  const form = document.createElement("form");
+  form.id = id;
+
+  return form;
+}
+
+function createTextBoxInput(text, id) { 
+  const formGroup = document.createElement("div");
+  
+  const formLineText = document.createElement("label");
+  formLineText.textContent = text;
+
+  const formInput = document.createElement("input");
+  formInput.id = id;
+  formLineText.htmlFor = id;
+
+  formGroup.appendChild(formLineText);
+  formGroup.appendChild(formInput);
+
+  return formGroup;
+}
+
+function createSelectInput(text, id, options) {
+  const formGroup = document.createElement("div");
+  
+  const formLineText = document.createElement("label");
+  formLineText.textContent = text;
+
+  const selectInput = document.createElement("select");
+  selectInput.id = id;
+
+  const selectOptions = options;
+
+  selectOptions.forEach((option) => {
+    const selection = document.createElement("option");
+    selection.value = option.value;
+    selection.textContent = option.text;
+    selectInput.appendChild(selection);
+  })
+
+  formLineText.htmlFor = id;
+
+  formGroup.appendChild(formLineText);
+  formGroup.appendChild(selectInput);
+
+  return formGroup;
+}
+
+function createDateInput(text, id) {
+  const formGroup = document.createElement("div");
+
+  const formLineText = document.createElement("label");
+  formLineText.textContent = text;
+
+  const dateInput = document.createElement("input");
+  dateInput.id = id;
+  dateInput.type = "date";
+
+  formGroup.appendChild(formLineText);
+  formGroup.appendChild(dateInput);
+
+  return formGroup;
 }
